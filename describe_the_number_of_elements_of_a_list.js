f6433893dfaa5acad03d087bd1a98df40a484b03
Ahraar Alam <ahraralam2012@gmail.com>
2021-06-18 02:08:38 +0530

    4th problem done

---
+++ describe_the_number_of_elements_of_a_list.js
@@ -0,0 +1,3 @@
+module.exports = function findLengthOfList(list) {
+  return list.length > 0 ? list.length : undefined;
+};
---
+++ test/describe_the_number_of_elements_of_a_list_Test.js
@@ -0,0 +1,27 @@
+const chai = require("chai");
+const expect = chai.expect;
+
+const findLengthOfList = require("../describe_the_number_of_elements_of_a_list");
+
+describe.only("find the Number of element in a list", () => {
+  context("when the list is empty", () => {
+    it("should return nothing", () => {
+      let result = findLengthOfList([]);
+      expect(result).to.be.undefined;
+    });
+  });
+
+  context("when the list contains one element", () => {
+    it("should return the first element", () => {
+      let result = findLengthOfList([100]);
+      expect(result).to.be.eql(1);
+    });
+  });
+
+  context("when the list contains many element", () => {
+    it("should return the length of list", () => {
+      let result = findLengthOfList([100, 101, 103]);
+      expect(result).to.be.eql(3);
+    });
+  });
+});
